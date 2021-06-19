0f3aadd48638bedf9b25a20691590461d3dbe4a5
KooHyeon <oldandnow9@gmail.com>
2021-06-20 02:11:45 +0900

    HTML&CSS: Project Page(Dungji) into

--- main.js
+++ main.js
@@ -6,54 +6,54 @@
 //     })
 
 // Logo Elipse Size
-function Init() {
-    let w = window.innerWidth;
-    let h = window.innerHeight * 0.8;
-    ellipse.setAttributeNS(null, 'viewBox', `0 0 ${w/0.65}  ${h/0.65}`);
-    let d = `M${9 * w / 10},${h / 2}
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${w / 10} ${5 * h / 10}
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} 
-  A${4 * w / 10},${4 * h / 10} 0 0ß 1 ${w / 10} ${5 * h / 10} 
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} `;
-    thePath.setAttributeNS(null, 'd', d);
+// function Init() {
+//     let w = window.innerWidth;
+//     let h = window.innerHeight * 0.8;
+//     ellipse.setAttributeNS(null, 'viewBox', `0 0 ${w/0.65}  ${h/0.65}`);
+//     let d = `M${9 * w / 10},${h / 2}
+//   A${4 * w / 10},${4 * h / 10} 0 0 1 ${w / 10} ${5 * h / 10}
+//   A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} 
+//   A${4 * w / 10},${4 * h / 10} 0 0ß 1 ${w / 10} ${5 * h / 10} 
+//   A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} `;
+//     thePath.setAttributeNS(null, 'd', d);
 
-    // Font size
-    let paths_length = thePath.getTotalLength();
-    text.style.fontSize = paths_length / 50;
-}
-window.setTimeout(function () {
-    Init();
-    window.addEventListener('road', Init);
-}, 15);
+//     // Font size
+//     let paths_length = thePath.getTotalLength();
+//     text.style.fontSize = paths_length / 50;
+// }
+// window.setTimeout(function () {
+//     Init();
+//     window.addEventListener('road', Init);
+// }, 15);
         
 
-// Logo Text Rotate
-let startText = 0;
-let timer = null;
+// // Logo Text Rotate
+// let startText = 0;
+// let timer = null;
 
-function textRotate() {
-    requestAnimationFrame(textRotate);
-    text.setAttributeNS(null, 'startOffset', startText + '%');
-    if (startText >= 50) {
-        startText = 0;
-    }
-    startText += 0.02;
-    console.log('stop');
-}
+// function textRotate() {
+//     requestAnimationFrame(textRotate);
+//     text.setAttributeNS(null, 'startOffset', startText + '%');
+//     if (startText >= 50) {
+//         startText = 0;
+//     }
+//     startText += 0.02;
+//     console.log('stop');
+// }
 
-textRotate()
+// textRotate()
 
-const loader = document.querySelector('.loader');
+// const loader = document.querySelector('.loader');
 
-window.addEventListener('load', () => {
-    setTimeout(fadeOut, 3000)
-});
+// window.addEventListener('load', () => {
+//     setTimeout(fadeOut, 3000)
+// });
 
-function fadeOut() {
-    loader.style.transition = '0.8s';
-    loader.style.opacity = 0;
-    loader.style.display = 'none';
-}
+// function fadeOut() {
+//     loader.style.transition = '0.8s';
+//     loader.style.opacity = 0;
+//     loader.style.display = 'none';
+// }
 
 // -------- navbar --------
 
