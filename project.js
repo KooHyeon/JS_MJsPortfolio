315f9d786e933602b05dcd883b6702d0904be877
KooHyeon <oldandnow9@gmail.com>
2021-07-02 13:22:58 +0900

    All: Separate JS file

--- project.js
+++ project.js
@@ -1,50 +1,3 @@
-// Logo Elipse Size
-function Init() {
-    let w = window.innerWidth;
-    let h = window.innerHeight * 0.8;
-    ellipse.setAttributeNS(null, 'viewBox', `0 0 ${w/0.65}  ${h/0.65}`);
-    let d = `M${9 * w / 10},${h / 2}
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${w / 10} ${5 * h / 10}
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} 
-  A${4 * w / 10},${4 * h / 10} 0 0ß 1 ${w / 10} ${5 * h / 10} 
-  A${4 * w / 10},${4 * h / 10} 0 0 1 ${9 * w / 10} ${5 * h / 10} `;
-    thePath.setAttributeNS(null, 'd', d);
-
-    // Font size
-    let paths_length = thePath.getTotalLength();
-    text.style.fontSize = paths_length / 50;
-}
-
-window.setTimeout(function () {
-    Init();
-    window.addEventListener('load', Init);
-}, 15);
-
-
-// Logo Text Rotate
-let startText = 0;
-let timer = null;
-
-function textRotate() {
-    requestAnimationFrame(textRotate);
-    text.setAttributeNS(null, 'startOffset', startText + '%');
-    if (startText >= 50) {
-        startText = 0;
-    }
-  startText += 0.02;
-}
-
-textRotate()
-
-const loader = document.querySelector('.loader');
-
-window.addEventListener('load', () => {
-  document.body.classList.remove('before-load');
-});
-
-loader.addEventListener('transitionend', (e) => {
-  document.body.removeChild(e.currentTarget);
-})
 
 //-------- navbar --------
 
@@ -127,3 +80,42 @@ function arrowDisappear() {
 function toTheTop() {
     window.scrollTo(0, 0);
 }
+
+// -------- project -------- 
+
+const leftBtn = document.querySelector('.sidearea--left button');
+const rightBtn = document.querySelector('.sidearea--right button');
+
+window.addEventListener('scroll', sideBtnDisappear);
+
+function sideBtnDisappear() {
+    if (window.scrollY > 200) {
+      leftBtn.style.opacity = 1 - window.scrollY / 400;
+      rightBtn.style.opacity = 1 - window.scrollY / 400;
+    } else if (window.scrollY < 10) {
+      leftBtn.style.opacity = 1;
+      rightBtn.style.opacity = 1;
+    }
+}
+const leftArea = document.querySelector('.sidearea--left');
+const rightArea = document.querySelector('.sidearea--right');
+
+leftArea.addEventListener('mouseover', leftBtnAppear);
+leftArea.addEventListener('mouseout', leftBtnDisappear);
+rightArea.addEventListener('mouseover', rightBtnAppear);
+rightArea.addEventListener('mouseout', rightBtnDisappear);
+
+function leftBtnAppear() {
+  leftBtn.style.opacity = 1
+}
+function leftBtnDisappear() {
+  if (window.scrollY > 10) leftBtn.style.opacity = 0;
+}
+function rightBtnAppear() {
+  rightBtn.style.opacity = 1;
+}
+function rightBtnDisappear() {
+  if (window.scrollY > 10) rightBtn.style.opacity = 0;
+}
+
+
